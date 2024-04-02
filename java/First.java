fb33a9045efe3d4d217ae46af4c5349cff30a608
Umesh Tiwari <klau.tiwari1234@gmail.com>
2024-04-02 10:34:31 +0530

    first program is Updated
    here we removed the constructor class and object and added the simple program which shows the average value of two different variables.

--- java/First.java
+++ java/First.java
@@ -1,19 +1,22 @@
 class First {
-    public String name;
-    public int age;
 
-    First(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-    public void display() {
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-    }
+    // this is just for practive changes in github
+
+
 
     public static void main(String[] args) {
-        First first = new First("Umesh", 21);
-        first.display();
+
+        /* here a is the first variable which
+         * has value 10 and b is another variable which has the value
+         * 20. which is greater then the value of the first
+         * varible.
+         */
+
+        int a = 10;
+        int b = 20;
+
+        double average = a + b / 2;
+        System.out.printl("Average value is: " + average);
     }
 
 }
